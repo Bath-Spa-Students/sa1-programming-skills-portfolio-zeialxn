0c07d6563c1fd4eec2cd4ac99291cddae40a3bb3
Zhealene Lucero <eihracalibara@gmail.com>
2024-11-21 10:04:55 +0400

    Strings Concatenation.py

---
+++ z_Bonus_Exercises/Strings Concatenation.py
@@ -0,0 +1,9 @@
+# Write three strings in different variables and print the output as one string.
+
+# Three strings in different variables
+a = "I"
+b = "Love"
+c = "You"
+
+# Print the output as one string
+print(a, b, c)
