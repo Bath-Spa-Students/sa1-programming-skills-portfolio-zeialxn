348132acb3f77b47df72f26103b955cb930686b1
Zhealene Lucero <eihracalibara@gmail.com>
2024-11-21 10:28:41 +0400

    List - Names.py

---
+++ z_Bonus_Exercises/List - Names.py
@@ -0,0 +1,8 @@
+# Store the names of a few of your friends in a list called names. Print each person’s name by accessing each element in the list, one at a time.
+
+# list
+names = ["Sey", "Thati", "Elisha"]
+
+# print
+for friends in names:
+    print(friends)
