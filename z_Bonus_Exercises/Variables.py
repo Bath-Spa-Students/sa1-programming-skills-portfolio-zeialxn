a4abe9caab10f550ed24be729aa5ea591dd6627a
Zhealene Lucero <eihracalibara@gmail.com>
2024-11-21 10:10:06 +0400

    Variables.py

---
+++ z_Bonus_Exercises/Variables.py
@@ -0,0 +1,13 @@
+# Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
+
+# assign a message to a variable
+name = "Axl Malibiran"
+
+# print the message
+print(name)
+
+# change the value of the variable to a new message
+name = "Axl Soriano"
+
+# print the new message
+print(name)
